20faedfefe6f6d14b75f9597eddf7ca6ef84c2eb
Esteban Mayoral Poncelas <emponcelas@izertis.com>
2023-11-10 11:16:58 +0100

    Force platform

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t my-golang-cross-compile .
-docker run --rm -v $(pwd):/app my-golang-cross-compile /app/compile.sh
+docker build -t daranix/golang-cross-compile .
+docker run --platform="linux/amd64" -it --rm -v $(pwd):/app daranix/golang-cross-compile /app/compile.sh
